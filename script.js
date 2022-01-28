28cfc2028b7a597cb0f381d63f28fa840a5f82cc
Ori Haham <ori.haham100@gmail.com>
2022-01-29 01:03:24 +0200

    feat/initialCards

--- script.js
+++ script.js
@@ -7,6 +7,34 @@ let editButton = document.querySelector(".profile__edit-button");
 let editName = form.querySelector(".profile-form__name");
 let editAboutMe = form.querySelector(".profile-form__about-me");
 let submitProfile = form.querySelector(".profile-form__submit-button");
+let cardsContainer = document.querySelector(".cards");
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
 
 function openPopup() {
   popup.classList.remove("popup_hidden");
@@ -28,3 +56,16 @@ function editProfile(event) {
 editButton.addEventListener("click", openPopup);
 popupClose.addEventListener("click", clocePopup);
 form.addEventListener("submit", editProfile);
+
+function createCard(cardsData) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").setAttribute("src", cardsData.link);
+  cardElement.querySelector(".card__location").textContent = cardsData.name;
+
+  return cardElement;
+}
+
+initialCards.forEach((card) => {
+  cardsContainer.append(createCard(card));
+});
